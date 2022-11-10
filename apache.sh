87dd85f05db69d4f0e9740fd72be7ffdccc79074
mehmet <eminunsal20@gmail.com>
2022-11-10 21:59:44 +0100

    apache.sh updated

--- apache.sh
+++ apache.sh
@@ -4,6 +4,6 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-sudo chmod -R 777 /var/www/html
+chmod -R 777 /var/www/html
 echo 'Hello Everyone.This is my WebSite' > /var/www/html/index.html
-sudo systemctl restart httpd
+systemctl restart httpd

31fae7bbc79c18300333ae18096c096568d644cb
mehmet <eminunsal20@gmail.com>
2022-11-18 20:07:48 +0100

    terraform.sh upload

---
+++ terraform.sh
@@ -0,0 +1,15 @@
+# Wenn man irgendwo Terraform-Tools installieren will,klicke auf https://developer.hashicorp.com/terraform/downloads 
+
+#! /bin/bash
+
+yum update -y
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
+mkdir terraform
+cd terraform
+touch main.tf
+
+
+
+
